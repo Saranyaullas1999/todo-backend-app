4a25c56549079b650a64c90cd09f90642924507d
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-25 14:44:32 +0530

    todo backend

---
+++ src/test/java/com/nestdigital/todobackendapp/TodoBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.todobackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
